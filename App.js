17b9c0babc1c38445760ec10ca7a619de0197b67
Jainik Patel <jainikhpatel@yahoo.com>
2019-08-03 20:49:55 +0530

    Removed 'ionicons' errors

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import { AppLoading } from 'expo';
 import { Asset } from 'expo-asset';
-import * as Font from 'expo-font';
+//import * as Font from 'expo-font';
 import React, { useState } from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+//import { Ionicons } from '@expo/vector-icons';
 
 import AppNavigator from './navigation/AppNavigator';
 
@@ -34,13 +34,13 @@ async function loadResourcesAsync() {
       require('./assets/images/robot-dev.png'),
       require('./assets/images/robot-prod.png'),
     ]),
-    Font.loadAsync({
+    /* Font.loadAsync({
       // This is the font that we are using for our tab bar
       ...Ionicons.font,
       // We include SpaceMono because we use it in HomeScreen.js. Feel free to
       // remove this if you are not using it in your app
       'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-    }),
+    }), */
   ]);
 }
 
